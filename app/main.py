c28534d63ce11e71d0ecaec73cbf9aef08e6019d
Icy-13 <zorinms@mail.ru>
2024-08-05 04:10:40 +0500

    updated routers

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.api.v1.endpoints import get_solution_router, upload_solution_router
 
 app = FastAPI()
+app.include_router(get_solution_router)
+app.include_router(upload_solution_router)
