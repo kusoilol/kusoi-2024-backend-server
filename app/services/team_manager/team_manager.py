c52bc941027b68a05ed21d7549805461e70c1a9f
Icy-13 <zorinms@mail.ru>
2024-08-05 04:08:40 +0500

    TeamManager learned how to search solutions

--- app/services/team_manager/team_manager.py
+++ app/services/team_manager/team_manager.py
@@ -1,6 +1,7 @@
+from uuid import UUID
 import os
 from os.path import join as path_join
-from tempfile import SpooledTemporaryFile
+from typing import IO
 
 from .config import TEAMS_PATH, HISTORY_NAME
 from app.schemas import Language
@@ -12,32 +13,39 @@ class TeamManager:
 
     """
 
+    max_id: int
+
     @staticmethod
     def _get_files_in_dir(directory):
         return [f for f in os.listdir(directory) if os.path.isfile(path_join(directory, f))]
 
-    def _get_main_sol(self):
-        return TeamManager._get_files_in_dir(self.team_path)
+    def get_main_filename(self) -> list[str]:
+        return TeamManager._get_files_in_dir(self._team_path)
+
+    def get_main_solution(self):
+        if self.max_id == 0:
+            raise FileNotFoundError(f"Team {self.team_id} doesn't have any solutions yet")
+        return path_join(self._team_path, TeamManager._get_files_in_dir(self._team_path)[0])
 
     def _get_history_sols(self):
-        return TeamManager._get_files_in_dir(self.team_path)
+        return TeamManager._get_files_in_dir(path_join(self._team_path, HISTORY_NAME))
 
     def _get_all_files(self):
-        return self._get_main_sol(), self._get_history_sols()
+        return self.get_main_filename(), self._get_history_sols()
 
-    def __init__(self, team_id: int):
+    def __init__(self, team_id: UUID):
         self.team_id = team_id
-        self.team_path = path_join(TEAMS_PATH, str(self.team_id))
-        os.makedirs(path_join(self.team_path, HISTORY_NAME), exist_ok=True)
+        self._team_path = path_join(TEAMS_PATH, str(self.team_id))
+        os.makedirs(path_join(self._team_path, HISTORY_NAME), exist_ok=True)
         self.max_id = 0
         main_sol, history_sols = self._get_all_files()
         self.max_id = max(int(filename.split('.')[0]) for filename in (main_sol + history_sols + ["0."]))
 
-    def create_solution(self, file: SpooledTemporaryFile, language: Language):
-        current_main = self._get_main_sol()
+    def create_solution(self, file: IO[bytes], language: Language):
+        current_main = self.get_main_filename()
         filename = str(self.max_id + 1)
         extension = language.value
-        filepath = path_join(self.team_path, filename + extension)
+        filepath = path_join(self._team_path, filename + extension)
         try:
             with open(filepath, 'wb') as save:
                 save.write(file.read())
@@ -49,6 +57,20 @@ class TeamManager:
             if not file.closed:
                 file.close()
         if current_main:
-            os.rename(path_join(self.team_path, current_main[0]),
-                      path_join(self.team_path, HISTORY_NAME, current_main[0]))
+            os.rename(path_join(self._team_path, current_main[0]),
+                      path_join(self._team_path, HISTORY_NAME, current_main[0]))
         self.max_id += 1
+        return self.max_id
+
+    def get_solution(self, solution_id: int):
+        """
+        return filepath to solution with given id
+            or raise FileNotFoundError if such solution doesn't exist
+        """
+        main_sol, history_sols = self._get_all_files()
+        if main_sol and int(main_sol[0].split('.')[0]) == solution_id:
+            return path_join(self._team_path, main_sol[0])
+        for filename in history_sols:
+            if int(filename.split('.')[0]) == solution_id:
+                return path_join(self._team_path, HISTORY_NAME, filename)
+        raise FileNotFoundError(f"Team {self.team_id} doesn't have solution with id {solution_id}")
