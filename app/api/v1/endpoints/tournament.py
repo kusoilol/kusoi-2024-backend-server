af7946d58e4c4310ede11214db93f1f8d3e87e60
Icy-13 <zorinms@mail.ru>
2024-08-25 02:29:37 +0500

    changed output of tournament and toyed with docstrings

--- app/api/v1/endpoints/tournament.py
+++ app/api/v1/endpoints/tournament.py
@@ -13,13 +13,24 @@ router = APIRouter(prefix="/tournament")
 async def run_tournament(teams: List[UUID]) -> dict:
     """
     :param teams: participating teams
-    :return: dictionary: key - team_id, value = list[previous_score, delta]
+
+    :return: dictionary: key is team_id, value is a list of three items:
+
+        1. score before tournament
+
+        2. change of score during tournament
+
+        3. score after tournament
+
     """
 
     data = db.dump_scoreboard(teams)
     new_data = data
     for key, val in data.items():
-        new_data[key] = [val, 0]
+        new_data[key] = [val, 0, 0]
+    for team in teams:
+        if team not in new_data:
+            new_data[team] = [0, 0, 0]
     data = new_data
 
     for i in range(len(teams)):
@@ -27,14 +38,12 @@ async def run_tournament(teams: List[UUID]) -> dict:
             continue
         for j in range(i + 1, len(teams)):
             log, winner, game_id = await play(teams[i], teams[j])
-            if winner not in data:
-                data[winner] = [0, 0]
-
             data[winner][1] += 1
 
     for team, lst in data.items():
         if lst[1] != 0:
             db.inc_score(team, lst[1])
+        lst[2] = lst[1] + lst[0]
 
     return data
 
